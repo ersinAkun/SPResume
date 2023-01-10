50d111960cb9a957b5f179ec655540b16f570e7c
Ersin Akun <ersinakun34@gmail.com>
2023-01-10 07:40:30 +0300

    v2

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import About from './about/about';
-import Intro from './about/intro/intro';
+
 import Educations from './education/educations';
 import Experiences from './experiences/experiences';
 import GetInTouch from './getintouch/get-in-touch';
 import "./main.css";
-import SectionHeader from './section-header/section-header';
+
+import Testimonals from './testimonals/testimonals';
 
 const Main = () => {
   return (
     <div className="main-content pull-right">
         <About/>
         <Experiences/>
-        <GetInTouch/>
         <Educations/>
+        <Testimonals/>
+        <GetInTouch/>
     </div>
   )
 }
